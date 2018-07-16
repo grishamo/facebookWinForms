c0670be74395cf0a8d60949377940f9ff7344314
Gregory Mozias <grishamo@gmail.com>
2018-07-16 18:11:23 +0300

    Facebook login

--- C18 Ex01 Gregory 317612950 Mariya 321373136/Form1.Designer.cs
+++ C18 Ex01 Gregory 317612950 Mariya 321373136/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FbLogin = new System.Windows.Forms.Button();
+            this.LikesList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // FbLogin
+            // 
+            this.FbLogin.Location = new System.Drawing.Point(238, 26);
+            this.FbLogin.Name = "FbLogin";
+            this.FbLogin.Size = new System.Drawing.Size(134, 23);
+            this.FbLogin.TabIndex = 0;
+            this.FbLogin.Text = "Facebook Connect";
+            this.FbLogin.UseVisualStyleBackColor = true;
+            this.FbLogin.Click += new System.EventHandler(this.FbLogin_Click);
+            // 
+            // LikesList
+            // 
+            this.LikesList.FormattingEnabled = true;
+            this.LikesList.Location = new System.Drawing.Point(35, 95);
+            this.LikesList.Name = "LikesList";
+            this.LikesList.Size = new System.Drawing.Size(309, 264);
+            this.LikesList.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 467);
+            this.Controls.Add(this.LikesList);
+            this.Controls.Add(this.FbLogin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button FbLogin;
+        private System.Windows.Forms.ListBox LikesList;
     }
 }
 
--- C18 Ex01 Gregory 317612950 Mariya 321373136/Form1.cs
+++ C18 Ex01 Gregory 317612950 Mariya 321373136/Form1.cs
@@ -5,14 +5,71 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
 
 namespace C18_Ex01_Gregory_317612950_Mariya_321373136
 {
     public partial class Form1 : Form
     {
+        private User m_LoggedInUser;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            if (ApplicationSettings.Instance.AutoLogin)
+            {
+                autoLogin();
+            }
+        }
+        private void autoLogin()
+        {
+            LoginResult result = FacebookService.Connect(ApplicationSettings.Instance.AccessToken);
+            if (string.IsNullOrEmpty(result.ErrorMessage))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                Console.WriteLine(m_LoggedInUser);
+            }
+        }
+
+        private void loginAndInit()
+        {
+            /// Use the FacebookService.Login method to display the login form to any user who wish to use this application.
+            /// You can then save the result.AccessToken for future auto-connect to this user:
+            LoginResult result = FacebookService.Login("229133017717072",
+                "public_profile",
+                "groups_access_member_info",
+                "user_likes"
+                );
+            // These are NOT the complete list of permissions. Other permissions for example:
+            // "user_birthday", "user_education_history", "user_hometown", "user_likes","user_location","user_relationships","user_relationship_details","user_religion_politics", "user_videos", "user_website", "user_work_history", "email","read_insights","rsvp_event","manage_pages"
+            // The documentation regarding facebook login and permissions can be found here: 
+            // https://developers.facebook.com/docs/facebook-login/permissions#reference
+
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                ApplicationSettings.Instance.AccessToken = result.AccessToken;
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
+        }
+
+        private void FbLogin_Click(object sender, EventArgs e)
+        {
+            loginAndInit();
+        }
+
     }
+    
+
 }
