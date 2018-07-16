f64ad380f11b443210123b4a8b1e80e47791aa3c
Gregory Mozias <grishamo@gmail.com>
2018-07-16 12:01:19 +0300

    Add project files.

---
+++ C18 Ex01 Gregory 317612950 Mariya 321373136/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C18_Ex01_Gregory_317612950_Mariya_321373136
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
